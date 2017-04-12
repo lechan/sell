4d29cb163ca9bc08716d78df7f94a35f1b5cc5fa
lechan <lechan@126.com>
2017-04-12 22:15:56 +0800

    提交

---
+++ src/components/seller/seller.vue
@@ -0,0 +1,10 @@
+<template>
+	<div class="header">我是商家信息</div>
+</template>
+
+<script type="text/ecmascript-6">
+	export default {};
+
+</script>
+
+<style rel="stylesheet/stylus" lang="stylus"></style>
